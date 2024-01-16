f77dd5d30330a6fb9878ab9423c888c81fd5c240
Gunther Tonitz <gtonitz@gmail.com>
2024-01-16 21:43:15 +0200

    Added HUD with helath and 3 resources. Resource pickup also displays

---
+++ Game/Scripts/UI/ui.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+@export var player: CharacterBody2D
+
+@onready var player_health_bar = %PlayerHealthBar
+
+@onready var rock_label = %RockLabel
+@onready var ore_label = %OreLabel
+@onready var wood_label = %WoodLabel
+
+var num_wood = 0:
+	set(new_wood):
+		num_wood = new_wood
+		update_wood_label()
+
+var num_rock = 0:
+	set(new_rock):
+		num_rock = new_rock
+		update_rock_label()
+
+var num_ore = 0:
+	set(new_ore):
+		num_ore = new_ore
+		update_ore_label()
+
+func _ready():
+	await player.ready
+	player.health_changed.connect(update_health_progress())
+	update_health_progress()
+	update_rock_label()
+	update_ore_label()
+
+func update_health_progress():
+	player_health_bar.value = player.health * 100 / player.max_health
+
+func update_wood_label():
+	wood_label.text = str(num_wood)
+
+func update_rock_label():
+	rock_label.text = str(num_rock)
+	
+func update_ore_label():
+	ore_label.text = str(num_ore)
+
+func _on_wood_tree_gathered_wood():
+	num_wood += 1
+
+func _on_rock_stone_gathered_stone():
+	num_rock += 1
+
+func _on_rock_ore_gathered_ore():
+	num_ore += 1
---
+++ Game/Scripts/rock_ore.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var is_ready : bool = false
+var is_player_nearby : bool = false
+
+signal gathered_ore
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if is_player_nearby:
+		if Input.is_action_just_pressed("interact"):
+			gathered_ore.emit()
+			queue_free()
+
+func _on_pickable_area_body_entered(body):
+	if body.name == "Player":
+		is_player_nearby = true
+
+func _on_pickable_area_body_exited(body):
+	if body.name == "Player":
+		is_player_nearby = false
