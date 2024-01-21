b2c83b7aab01241cf0181ce3086815b6b308e812
Gunther Tonitz <gtonitz@gmail.com>
2024-01-21 16:03:05 +0200

    Added new player and fixed some animation bugs. Also resized collision shapes. Finally, added lines to gitignore to ignore import files.

--- Game/Scripts/Player/Attack.gd
+++ Game/Scripts/Player/Attack.gd
@@ -8,6 +8,7 @@ extends State
 var direction = Vector2.ZERO
 
 func enter(msg := {}) -> void:
+	print("Player entering ATTACK state")
 	if msg.has("direction"):
 		direction = msg.direction
 
@@ -29,33 +30,45 @@ func update(_delta: float) -> void:
 		state_machine.transition_to("Idle", {direction = direction})
 
 func play_animation():
-	
 	if direction == Vector2.ZERO:
-		animated_sprite_2d.play("attack_down")
+		animated_sprite_2d.play("idle_down")
+		return
 	
-	match direction.y:
-		-1.0:
-			animated_sprite_2d.play("attack_up")
-		1.0:
+	if abs(direction.y) >= abs(direction.x):
+		if direction.y >= 0:
 			animated_sprite_2d.play("attack_down")
-	match direction.x:
-		-1.0:
-			animated_sprite_2d.play("attack_left")
-		1.0:
-			animated_sprite_2d.play("attack_right")
+		else:
+			animated_sprite_2d.play("attack_up")
+		return
+
+	if direction.x >= 0:
+		animated_sprite_2d.play("attack_right")
+	else:
+		animated_sprite_2d.play("attack_left")
 	
-	if direction.x > 0.5 and direction.y < -0.5:
-		animated_sprite_2d.play("attack_right_up")
-	if direction.x > 0.5 and direction.y > 0.5:
-		animated_sprite_2d.play("attack_right_down")
-	if direction.x < -0.5 and direction.y < -0.5:
-		animated_sprite_2d.play("attack_left_up")
-	if direction.x < -0.5 and direction.y > 0.5:
-		animated_sprite_2d.play("attack_left_down")
+	#if direction.x > 0.5 and direction.y < -0.5:
+		#animated_sprite_2d.play("attack_right_up")
+	#if direction.x > 0.5 and direction.y > 0.5:
+		#animated_sprite_2d.play("attack_right_down")
+	#if direction.x < -0.5 and direction.y < -0.5:
+		#animated_sprite_2d.play("attack_left_up")
+	#if direction.x < -0.5 and direction.y > 0.5:
+		#animated_sprite_2d.play("attack_left_down")
 
 func adjust_hitbox():
 	var offset = Vector2(2, 0)
 	var rotation = direction.angle()
-	
+
+	# Determine the predominant direction
+	if abs(direction.x) <= abs(direction.y):
+		# Vertical movement is more significant, prioritize up or down
+		if direction.y < 0:
+			rotation = deg_to_rad(270)  # Rotate downwards
+		else:
+			rotation = deg_to_rad(90)  # Rotate upwards
+	else:
+		# Horizontal movement is more significant, use the original rotation
+		rotation = deg_to_rad(round(rad_to_deg(rotation) / 90.0) * 90.0)
+
 	sword_hitbox.global_position = owner.global_position + offset.rotated(rotation)
 	sword_hitbox.rotation = rotation
