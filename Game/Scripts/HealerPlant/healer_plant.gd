482420a93b267fd103dc8fb00e8523705580db06
Neil Brincker <neilbrincker77@gmail.com>
2024-01-21 17:41:34 +0200

    Healing mechanic working now

--- Game/Scripts/HealerPlant/healer_plant.gd
+++ Game/Scripts/HealerPlant/healer_plant.gd
@@ -6,42 +6,95 @@ extends Node2D
 
 #Variables
 var target
+var can_heal_enemy = false
+var waiting_for_process = false
 @onready var heal_timer = $HealTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if waiting_for_process:
+		return
+	if target == null:
+		_find_new_target()
+		print("New target found: ", target)
+		
+		waiting_for_process = true
+		$WaitTimer.start()
+		
+	if target != null:
+		waiting_for_process = true
+		$WaitTimer.start()
+		if target.name == "MeleePlant":
+			if $HealRange.overlaps_body(target) && can_heal_enemy:
+				heal_timer.start()
+				can_heal_enemy = false
+				print("Timer started")
+		if target.name == "RangedPlant":
+			if $HealRange.overlaps_body(target.get_parent()) && can_heal_enemy:
+				heal_timer.start()
+				can_heal_enemy = false
 
 func _on_vision_range_body_entered(body):
-	if body is CharacterBody2D:
-		if body.is_in_group("Enemies"):
-			if body.name == "RangedPlant":
-				print("Ranged in vision range")
-				print(body.get_parent().HEALTH)
-			if body.name == "MeleePlant":
-				print("Melee in vision range")
-				print(body.HEALTH)
+	if target == null:
+		print("Target entered vision range")
+		_find_new_target()
+
+func _on_vision_range_body_exited(body):
+	if _same_target(body):
+		print("Target exited vision range")
+		target = null
+		_find_new_target()
 
 func _on_heal_range_body_entered(body):
-	if body.is_in_group("Enemies"):
+	if _same_target(body):
+		print("Target entered healing range")
 		heal_timer.start()
-		if body.name == "RangedPlant":
-			target = body.get_parent()
-		if body.name == "MeleePlant":
-			target = body
 
 func _on_heal_range_body_exited(body):
-	heal_timer.stop()
+	if _same_target(body):
+		print("Target exited healing range")
+		heal_timer.stop()
 
 func _on_heal_timer_timeout():
-	_heal_current_target(target)
+	print("Timer timeout")
+	if target != null:
+		print("------------------------------------------")
+		print("Target to heal: ", target)
+		print("Target health: ", target.HEALTH)
+		print("------------------------------------------")
+		_heal_current_target(target)
 
 func _heal_current_target(heal_target):
-	print("Health of target: ", heal_target.HEALTH)
 	if heal_target.HEALTH < heal_target.MAX_HEALTH:
 		heal_target.HEALTH += HEAL
+	else:
+		target = null
+		heal_timer.stop()
+
+func _find_new_target():
+	for body in $VisionRange.get_overlapping_bodies():
+		if body is CharacterBody2D && body.name != "HealerPlant":
+			if body.name == "RangedPlant":
+				if body.get_parent().HEALTH < body.get_parent().MAX_HEALTH:
+					target = body.get_parent()
+					can_heal_enemy = true
+			if body.name == "MeleePlant":
+				if body.HEALTH < body.MAX_HEALTH:
+					target = body
+					can_heal_enemy = true
+
+func _same_target(body):
+	if body.name == "RangedPlant":
+		if body.get_parent() == target:
+			return true
+	if body.name == "MeleePlant":
+		if body == target:
+			return true
+
+
+func _on_wait_timer_timeout():
+	waiting_for_process = false
