4c5822f5b40550578f5187d9f1a3d382066c17df
Begemann, CW, Mr [24791334@sun.ac.za] <24791334@sun.ac.za>
2024-01-22 19:09:20 +0200

    added healthbars

--- Game/Scenes/purifying_tower.gd
+++ Game/Scenes/purifying_tower.gd
@@ -1,10 +1,13 @@
 extends Node2D
 @export var HEALTH = 10
+const MAX_HEALTH = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$HealthBar.value=100
 
+func updateHealthbar():
+	$HealthBar.value =HEALTH*100/MAX_HEALTH
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
