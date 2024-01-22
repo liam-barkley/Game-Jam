d6461186dbcca661caed53f65201997e24d4ca11
Neil Brincker <neilbrincker77@gmail.com>
2024-01-22 20:06:12 +0200

    Melee searching for closest enemy

--- Game/Scripts/MeleeEnemy/melee_plant.gd
+++ Game/Scripts/MeleeEnemy/melee_plant.gd
@@ -10,20 +10,24 @@ extends CharacterBody2D
 
 # Variables
 var hurt_area
+var target
+var waiting_for_process = false
 
 func _physics_process(_delta):
-	pass
+	if waiting_for_process:
+		return
+	
+	await _find_closest_target()
+	
+	waiting_for_process = true
+	$WaitTimer.start()
 
 func _on_vision_range_body_entered(body):
 	if body.name == "Player":
-		#player = body
-		#mobile = true
 		melee_enemy_state_machine.transition_to("Move", {"player" = body})
 
 func _on_vision_range_body_exited(body):
 	if body.name == "Player":
-		#mobile = false
-		#player = null
 		var direction = position.direction_to(body.position)
 		melee_enemy_state_machine.transition_to("Idle", {"direction" = direction})
 
@@ -57,11 +61,31 @@ func _on_attack_timer_timeout():
 func _on_hurtbox_area_entered(area):
 	if area.is_in_group("Weapons") :
 		HEALTH -= 2
-		
-		
+
 	if area.is_in_group("Abullets"):
 		HEALTH -= 1
+
 	if HEALTH <=0:
-			print("imfree")
-			#get_parent().num_enemies -= 1
-			queue_free()
+		get_parent().num_enemies -= 1
+		queue_free()
+
+func _find_closest_target():
+	var closest_enemy = null
+	var shortest_distance = 100000000000
+	for body in $VisionRange.get_overlapping_bodies():
+		if body is CharacterBody2D || body is StaticBody2D && !body.is_in_group("Enemies"):
+			var body_position = body.global_position
+			var distance = global_position.distance_to(body_position)
+			if distance < shortest_distance:
+				shortest_distance = distance
+				closest_enemy = body
+	if closest_enemy:
+		target = closest_enemy
+		print("--------------------------------")
+		print("Closest enemy found: ", target.name)
+		print("--------------------------------")
+	else:
+		target = null
+
+func _on_wait_timer_timeout():
+	waiting_for_process = false
